2f2b80b4d1c12e6871d145694892f8ae57e076e7
liuyuexong <851046264@qq.com>
2020-07-30 15:06:05 +0800

    第一个输出
    Signed-off-by: liuyuexong <851046264@qq.com>

---
+++ src/main/java/org/ym/entry/User.java
@@ -0,0 +1,15 @@
+package org.ym.entry;
+
+/**
+ * @author:liuyuexiong
+ * @mail:851046264@qq.com
+ * @date:2020/07/30
+ * @time:14/59
+ */
+
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("第一个Maven");
+    }
+}
